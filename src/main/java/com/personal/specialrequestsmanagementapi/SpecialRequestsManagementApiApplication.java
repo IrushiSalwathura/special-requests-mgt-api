0f355486513951434be259493d693189c6fd766b
IrushiSalwathura <irushitharindi@gmail.com>
2025-07-22 16:38:37 +0530

    Initial commit :fire:

---
+++ src/main/java/com/personal/specialrequestsmanagementapi/SpecialRequestsManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.personal.specialrequestsmanagementapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpecialRequestsManagementApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpecialRequestsManagementApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/personal/specialrequestsmanagementapi/SpecialRequestsManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal.specialrequestsmanagementapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpecialRequestsManagementApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
